b02394c86776c5665ebf8aa2c162b296de3ff01d
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2023-04-27 11:17:04 +0530

    issue solved...

--- employee-service/src/main/java/com/daadestroyer/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/com/daadestroyer/employeeservice/service/APIClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT SERVICE")
+@FeignClient(url = "http://localhost:8080",name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("/get-dept-by-code/{deptCode}")
     public DepartmentDto getDeptByCode(@PathVariable String deptCode);
